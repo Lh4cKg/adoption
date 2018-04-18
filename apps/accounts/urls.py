b4fb4bba9bc64a5631bd7c3e28f1d94de7bbcc71
Zarko Timarac <timaraczarko@gmail.com>
2018-04-18 20:37:14 +0200

    some little fixes

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -15,9 +15,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name= "login.html", ), name= "login"),
     path('logout/', LogoutView.as_view(template_name = 'logout.html'),name= "logout"),
     path('register/', signup, name ="register"),
-    # path('<nickname>', user_profile, name = 'profile'),
+    path('<int:pk>', ProfileDetail.as_view(), name = 'profile'),
     path('<nickname>/update/', ProfileUpdate.as_view(), name = 'update'),
-    path('<nickname>',user_profile, name = 'detail'),
+    # path('<nickname>',user_profile, name = 'detail'),
     # path('messages/',include(django_messages_urls), name = 'messages')
 
 
